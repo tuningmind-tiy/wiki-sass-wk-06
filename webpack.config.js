5ab4880ad665fcb0114e16bf14186873e86def52
tuningmind <tuningmind@gmail.com>
2017-04-04 14:08:10 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path')
+
+module.exports = {
+  entry: ['./app/index.js', './app/styles/main.sass'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  module: {
+    rules: [{
+      test: /\.sass$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }]
+  },
+  devtool: 'cheap-eval-source-map'
+}
+
